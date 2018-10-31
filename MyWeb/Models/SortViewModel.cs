adc6f2bec117013a22eae868b094d0201d33b532
ctok81 <savin-m-s-a@yandex.ru>
2018-10-31 17:08:54 +0500

    Добавлена пагинация

---
+++ MyWeb/Models/SortViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWeb.Models
+{
+    public class SortViewModel
+    {
+        public SortState NameSort { get; private set; }
+        public SortState AgeSort { get; private set; }
+        public SortState CompanySort { get; private set; }
+        public SortState Current { get; private set; }
+
+        public SortViewModel(SortState sortOrder)
+        {
+            NameSort = sortOrder == SortState.NameAsc ? SortState.NameDesc : SortState.NameAsc;
+            AgeSort = sortOrder == SortState.AgeAsc ? SortState.AgeDesc : SortState.AgeAsc;
+            CompanySort = sortOrder == SortState.CompanyAsc ? SortState.CompanyDesc : SortState.CompanyAsc;
+            Current = sortOrder;
+        }
+    }
+}
